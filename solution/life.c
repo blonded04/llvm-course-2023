e6975b3415ac5971612dc2fa264029ed5185b118
blonded04 <generalretcher@gmail.com>
2023-10-09 17:10:56 +0300

    rename variables

--- solution/life.c
+++ solution/life.c
@@ -1,43 +1,47 @@
 #include "sim.h"
 #define REPEAT_SIZE 1024
 
-void drawGen(unsigned* gen) {
+void drawGeneration(unsigned* generation) {
     unsigned x = 0;
     unsigned y = 0;
     for (y = 0; y < SIM_Y_SIZE; y++) {
         for (x = 0; x < SIM_X_SIZE; x++) {
-            simSetPixel(x, y, 0xFF000000 + 0xFF00 * gen[y * SIM_X_SIZE + x]);
+            simSetPixel(x, y,
+                        0xFF000000 + 0xFF00 * generation[y * SIM_X_SIZE + x]);
         }
     }
     simFlush();
 }
 
-void calcGen(unsigned* nextGen, unsigned* prevGen) {
-    // TBD
+void calculateNextGeneration(unsigned* currentGeneration,
+                             unsigned* nextGeneration) {
+    // TODO
 }
 
 int main() {
     unsigned x = 0;
     unsigned y = 0;
     unsigned i = 0;
-    unsigned gen1[SIM_Y_SIZE * SIM_X_SIZE];
-    unsigned gen2[SIM_Y_SIZE * SIM_X_SIZE];
-    unsigned* nextGen = gen1;
-    unsigned* prevGen = gen2;
+    unsigned generation1[SIM_Y_SIZE * SIM_X_SIZE];
+    unsigned generation2[SIM_Y_SIZE * SIM_X_SIZE];
+    unsigned* nextGeneration = generation1;
+    unsigned* prevGeneration = generation2;
 
     for (y = 0; y < SIM_Y_SIZE; y++) {
         for (x = 0; x < SIM_X_SIZE; x++) {
-            prevGen[y * SIM_X_SIZE + x] = simRand() % 2;
+            prevGeneration[y * SIM_X_SIZE + x] = simRand() % 2;
         }
     }
-    drawGen(prevGen);
+    drawGeneration(prevGeneration);
 
     for (i = 0; i < REPEAT_SIZE; i++) {
-        calcGen(nextGen, prevGen);
-        drawGen(nextGen);
-        unsigned* tmp = prevGen;
-        prevGen = nextGen;
-        nextGen = tmp;
+        calculateNextGeneration(nextGeneration, prevGeneration);
+        drawGeneration(nextGeneration);
+
+        unsigned* tmp = prevGeneration;
+        prevGeneration = nextGeneration;
+        nextGeneration = tmp;
     }
+
     return 0;
 }
